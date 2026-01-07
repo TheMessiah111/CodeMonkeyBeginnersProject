d521d3a136590a948d47b0c3cbda90a540882461
Daniel Promise <promizzdaniel@gmail.com>
2026-01-07 10:36:58 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private int moveSpeed = 1;
+    [SerializeField] private List<Coin> coinList;
+    [SerializeField] private List<Transform> coinTowerList;
+    private float minDistance = 1f;
+    private Vector3 playerTransform;
+    [SerializeField] private TextMeshProUGUI coinText;
+    private int noOfCoin;
+    void Start()
+    {
+        noOfCoin = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       MovePlayer();
+       DestroyCoinOnSpace();
+
+    }
+
+    private void Towers()
+    {
+        
+    }
+
+    private void AddCoin()
+    {
+        noOfCoin += 1;
+    }
+
+    private void DestroyCoinOnSpace()
+    {
+            foreach (Coin coin in coinList)
+        {
+           if (Input.GetKeyDown(KeyCode.Space))
+            {
+                 if(coin != null)
+            {
+                playerTransform = transform.position;
+                if((playerTransform - coin.transform.position).magnitude < minDistance)
+                {
+                    Debug.Log("Magnitude: "+(playerTransform - coin.transform.position).magnitude);
+                    coin.DestroyCoin();
+                    AddCoin();
+                    Debug.Log("Number of Coins: " + noOfCoin);
+                }
+            }
+            }
+        }
+        
+    }
+
+    private void MovePlayer()
+    {
+         if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0,1,0) * moveSpeed;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0,-1,0) * moveSpeed;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1,0,0) * moveSpeed;
+        }
+         else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1,0,0) * moveSpeed;
+        }
+    }
+}
